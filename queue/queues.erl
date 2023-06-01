597d7726cf7c11a677b770ec6a8f5b6383a96063
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-06-01 09:15:04 -0600

    Added a queue len check, basic structure for queue

--- queue/queues.erl
+++ queue/queues.erl
@@ -1,27 +1,40 @@
 -module(queues). 
 -import(lists,[append/2]). 
--export([start/0, user_input/0, increase_queue/2]). 
-
+-export([start/0, user_input/0, add_to_queue/2, check_queue_len/1]). 
 
 
 start() -> 
     Lst1 = [1,2,3], 
-    % Lst2 = lists:append(Lst1, [4,5]),
     io:fwrite("~w~n", [Lst1]),
-    % io:fwrite("~w~n", [Lst2]),
 
     Term = user_input(),
-    increase_queue(Term, Lst1).
+    Lst3 = add_to_queue(Term, Lst1),
+    Check = check_queue_len(Lst3),
+    io:fwrite(Check).
+
 
 user_input() ->
     {ok, Term} = io:read("Enter a number: "),
     io:format("The number you entered is: ~w~n", [Term]),
     Term.
 
-increase_queue(Term, Lst1) ->    
-    Lst2 = lists:append(Lst1, [Term]),
-    io:fwrite("~w~n", [Lst2]).
 
-    % Lst1.
+add_to_queue(Term, Lst1) ->    
+    Lst2 = lists:append(Lst1, [Term]),
+    io:fwrite("~w~n", [Lst2]),
+    Lst2.
+
+
+check_queue_len(Lst3) ->
+    if 
+      length(Lst3) == 4 -> 
+        % io:fwrite("True\n"),
+        Check = true;
+        
+      true -> 
+        % io:fwrite("False\n"),
+        Check = false
+    end,
+    Check.
 
 
