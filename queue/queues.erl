76ada864c88f2a5bbc81d3649aee40041bade332
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-06-02 20:00:15 -0600

    Updaated queues, and queue_info

--- queue/queues.erl
+++ queue/queues.erl
@@ -1,49 +1,58 @@
--module(queues). 
--import(lists,[append/2, delete/2, nth/2]). 
--export([start/0, user_input/0, add_to_queue/2, check_queue_len/1, remove_var_queue/1]). 
+-module(queues).
+-import(lists, [delete/2, nth/2, append/2]).
+-export([start/0, remove_var_queue/1, increase_queue/2, user_input/0, output/1]).
+
+ 
+
+start() ->
+    Lst = [1,2,3],
+    output(Lst),
+    
+    UserInput = user_input(),
+    Lst6 = increase_queue(Lst, UserInput),
+    io:fwrite("New List: ~w~n", [Lst6]),
+
+    Lst5 = remove_var_queue(Lst6),
+    io:fwrite("Final List: ~w~n", [Lst5]).
 
 
-start() -> 
-    Lst1 = [1,2,3], 
-    io:fwrite("~w~n", [Lst1]),
-
-    Term = user_input(),
-    Lst2 = add_to_queue(Term, Lst1),
-    Check = check_queue_len(Lst2),
-    io:fwrite(Check),
-    Lst3 = remove_var_queue(Lst2).
-
-
-user_input() ->
-    {ok, Term} = io:read("Enter a number: "),
-    io:format("The number you entered is: ~w~n", [Term]),
-    Term.
-
-
-add_to_queue(Term, Lst1) ->    
-    Lst2 = lists:append(Lst1, [Term]),
-    io:fwrite("~w~n", [Lst2]),
-    Lst2.
 
 remove_var_queue(Lst) ->
-    Var = nth(1,Lst),
-    Lst3 = delete(Var, Lst),
-    % io:fwrite("~w~n\n", [Lst4]).
-    Lst3.
+    Term = nth(1, Lst),
+    io:fwrite("Term: ~w~n", [Term]),
+    io:fwrite("List: ~w~n", [Lst]),
 
+    Lst1 = lists:delete(Term, Lst),
 
-check_queue_len(Lst2) ->
-    % Queue len is set to 4
-    % Checks to see if the len of queue is at 4
-    if 
-      length(Lst2) == 4 -> 
-        % io:fwrite("True\n"),
-        Check = true;
-        
-      true -> 
-        % io:fwrite("False\n"),
-        Check = false
-    end,
-    Check.
+    Term1 = nth(1, Lst1),
+    io:fwrite("Term: ~w~n", [Term1]),
+    io:fwrite("List: ~w~n", [Lst1]),
 
+    Lst2 = lists:delete(Term1, Lst1),
+    
+    Term2 = nth(1, Lst2),
+    io:fwrite("Term: ~w~n", [Term2]),
+    io:fwrite("List: ~w~n", [Lst2]),
+
+    Lst3 = lists:delete(Term2, Lst2),  
+    
+    Term3 = nth(1, Lst3),
+    io:fwrite("Term: ~w~n", [Term3]),
+    io:fwrite("List: ~w~n", [Lst3]),
+
+    Lst7 =  lists:delete(Term3, Lst3),
+
+    Lst7.
+
+increase_queue(Lst, I) -> 
+    Lst4 = lists:append(Lst, [I]),
+    Lst4. 
+
+user_input() -> 
+    {ok, I} = io:read("Enter a number: "),
+    io:format("The number you enterd is: ~w~n", [I]),
+    I.
+
+output(Lst) -> 
+    io:fwrite("~w~n", [Lst]).
 
--- tutorial/recursion.erl
+++ tutorial/recursion.erl
@@ -1,4 +1,15 @@
--module(recursion). 
+% -module(helloworld). 
+% -export([len/1,start/0]). 
+
+% len([]) -> 0; 
+% len([_|T]) -> 1 + len(T). 
+
+% start() -> 
+%    X = [1,2,3,4], 
+%    Y = len(X), 
+%    io:fwrite("~w",[Y]).
+
+-module(helloworld). 
 -export([fac/1,start/0]). 
 
 fac(N) when N == 0 -> 1; 
@@ -6,4 +17,4 @@ fac(N) when N > 0 -> N*fac(N-1).
 
 start() -> 
    X = fac(4), 
-   io:fwrite("~w\n",[X]).
+   io:fwrite("~w",[X]).
